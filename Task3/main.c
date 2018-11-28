f26de2298994e355c229ea085754c0e01d6ad0a1
PerJNilsson <per90nilsson@gmail.com>
2018-11-28 10:41:40 +0100

    new hopefully working

--- Task3/main.c
+++ Task3/main.c
@@ -13,7 +13,7 @@ int main() {
   int nbr_of_simulations = 8;
   int N[nbr_of_simulations];
   double random_value;
-  double mean_value;
+  double I_value;
   double variance;
   double value_of_integral;
   double error;
@@ -28,7 +28,7 @@ int main() {
   double x1,x2,y1,y2,z1,z2;
   srand(time(NULL));
   for (int i=0; i<nbr_of_simulations-1; i++){
-    mean_value=0;
+    I_value=0;
     variance=0;
     count = 0;
     x1=0.1; y1=-0.1; z1=0.5;
@@ -36,13 +36,16 @@ int main() {
     for (int j=0; j<N[i]; j++){
       random_value = (double) rand() / (double) RAND_MAX;
       x2 = trial_change(x1, random_value);
+      random_value = (double) rand() / (double) RAND_MAX;
       y2 = trial_change(y1, random_value);
+      random_value = (double) rand() / (double) RAND_MAX;
       z2 = trial_change(z1, random_value);
 
       w1 = weightfunction(x1,y1,z1);
       w2 = weightfunction(x2,y2,z2);
 
       random_value = (double) rand() / (double) RAND_MAX;
+
       if (random_value > w2/w1) {
         x2=x1; y2=y1; z2=z1;
         count+=1;
@@ -50,35 +53,33 @@ int main() {
       x1=x2; y1=y2; z1=z2;
 
       I = integral_function(x1,y1,z1);
-      mean_value += I;
-      variance += I*I;
+      I_value += I;
+      //variance += I*I;
     }
-    mean_value = mean_value / (double)(N[i]);
-    variance = variance / (double) N[i] - mean_value*mean_value;
+    I_value = I_value / (double)(N[i]);
+    variance = 0;//variance / (double) N[i] - mean_value*mean_value;
 
 
-    value_of_integral = mean_value + sqrt(variance/((double)N[i]));
+    value_of_integral = I_value;//mean_value + sqrt(variance/((double)N[i]));
     error = 0; //sqrt((value_of_integral-(1.0/6.0))*(value_of_integral-(1.0/6.0)))/(1.0/6.0);
-    printf("N=%09d | I=%f | error=%f | variance=%f | new states generated: %f\n",N[i], value_of_integral, error, variance,(double)count/(double)N[i]);
+    printf("N=%09d | I=%f | error=%f | variance=%f | new states: %f\n",N[i], value_of_integral, error, variance,1.0-(double)count/(double)N[i]);
   }
-  
   return 0;
 }
 
 double trial_change (double x_m, double r) {
   double x_n, C;
-  C = 2.5;
+  C = 2.00;
   return x_n = x_m+C*(r-0.5); 
 }
 
 double weightfunction(double x, double y, double z){
-  double w;
 
   return (1.0/sqrt(M_PI*M_PI*M_PI))*exp(-(x*x+y*y+z*z));
 }
 
 double integral_function(double x, double y, double z){
-  return (1.0/sqrt(M_PI*M_PI*M_PI))*exp(-(x*x+y*y+z*z))*(x*x+x*x*y*y+x*x*y*y*z*z);
+  return (x*x+x*x*y*y+x*x*y*y*z*z);
 }
 
 
